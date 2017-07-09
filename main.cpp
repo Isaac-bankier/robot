c205e99b7d3c8b1786e4614a2b99b735a3d471d9
isaac bankier <isaacbankier@gmail.com>
2017-07-09 16:21:14 +1000

    added the main file

---
+++ main.cpp
@@ -0,0 +1,36 @@
+//Main.cpp
+//
+//
+//
+//
+//
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
+
+void move_motor() {
+
+
+}
+
+void move_robot() {
+
+
+}
+
+void l(){
+
+
+}
+
+int get_ball(){
+
+
+}
