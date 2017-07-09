e79cb698eefc0db35c26d23ab332510af1254f97
isaac bankier <isaacbankier@gmail.com>
2017-07-10 07:24:42 +1000

    Added the main functions.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 //
 //
 
+int motor_strength(double Vlinear, double direction, double Vangular, int wheel) {
+  double wheels[3] = [0.0, 120.0, 240.0]
+  double wheelAngle = wheels[wheel]
+  double Vrx = Vlinear * cos(direction);
+  double Vry = Vlinear * sin(direction);
+  double Vwx = Vrx * cos(wheelAngle);
+  double Vwy = Vry * - sin(wheelAngle);
+  double Vw = Vlinear * (cos(direction) * cos(wheelAngle) - sin(direction) * sin(wheelAngle)) + Vangular
+
+}
+
 void setup() {
 
 
@@ -20,14 +31,12 @@ void move_motor() {
 
 }
 
-void move_robot() {
-
-
-}
-
-void l(){
-
-
+void move_robot(direction, speed) {
+  //direction is an int between -1 and 1
+  //speed is a float between 0 and 1
+  //
+  //
+  //
 }
 
 int get_ball(){
